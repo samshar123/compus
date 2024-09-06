0b9cb594087648113c52c2c0118deb05babdab48
Dotco <dotco@vmi2092978.contaboserver.net>
2024-09-06 18:24:05 +0200

    docker updating

--- core/settings.py
+++ core/settings.py
@@ -116,6 +116,10 @@ USE_I18N = True
 USE_TZ = True
 
 
+CSRF_TRUSTED_ORIGINS = [
+	"http://94.136.185.170:8081"
+]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
